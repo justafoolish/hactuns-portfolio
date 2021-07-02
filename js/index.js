4496b2aac5aac85afc7e6d6076b52d26cb3a6e73
Hắc Tún <hactuns@MacBook-Pro-cua-Hac.local>
2021-07-02 23:17:27 +0700

    to much space, card - logo animate

--- js/index.js
+++ js/index.js
@@ -3,10 +3,20 @@ document.querySelector(".navbar-toggler").addEventListener("click", function () 
     $(".navbar-toggler").toggleClass("is-menu-open");
   });
 
+const revealPosition = () => {
+    let windowHeight = window.innerHeight
+    let revealtop = element.getBoundingClientRect().top
+    let revealpoint = 100
+    
+    return revealtop - windowHeight < revealpoint
+}
+
 
 const reveal = () => {
     var fadeUp = document.querySelectorAll("[data-scroll=fade-up]");
     var fadeSide = document.querySelectorAll("[data-scroll=fade-left],[data-scroll=fade-right]");
+    var zoomOut = document.querySelectorAll("[data-scroll=zoom-out]");
+    
     fadeUp.forEach(element => {
 
         let windowHeight = window.innerHeight
@@ -33,6 +43,20 @@ const reveal = () => {
             element.classList.remove("fade-side")
         }
     })
+
+    zoomOut.forEach(element => {
+        let windowHeight = window.innerHeight
+        let revealtop = element.getBoundingClientRect().top
+        let revealpoint = 100
+
+        if(revealtop - windowHeight < revealpoint) {
+            element.classList.add("zoom-out")
+        }
+        else {
+            element.classList.remove("zoom-out")
+        }
+    })
+    
 
 }
 window.addEventListener("scroll", reveal)
