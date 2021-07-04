49bca82d12bf66c09f7595cdb39bf7da22fa14d3
Hắc Tún <hactuns@MacBook-Pro-cua-Hac.local>
2021-07-04 17:48:22 +0700

    reveal on first load only

--- js/index.js
+++ js/index.js
@@ -1,52 +1,31 @@
-
 document.querySelector(".navbar-toggler").addEventListener("click", function () {
-    $(".navbar-toggler").toggleClass("is-menu-open");
-  });
-
+  $(".navbar-toggler").toggleClass("is-menu-open");
+});
 
 const reveal = () => {
-    const fadeUp = document.querySelectorAll("[data-scroll=fade-up]");
-    const fadeSide = document.querySelectorAll("[data-scroll=fade-left],[data-scroll=fade-right]");
-    const zoomOut = document.querySelectorAll("[data-scroll=zoom-out]");
+  const fadeUp = document.querySelectorAll("[data-scroll=fade-up]");
+  const fadeSide = document.querySelectorAll("[data-scroll=fade-left],[data-scroll=fade-right]");
+  const zoomOut = document.querySelectorAll("[data-scroll=zoom-out]");
 
-    let revealPosition = (element) => {
-        let windowHeight = window.innerHeight
-        let revealtop = element.getBoundingClientRect().top
-        let revealpoint = 100
-        
-        return (revealtop - windowHeight) < revealpoint
-    }
-    
-    fadeUp.forEach(element => {
+  let revealPosition = (element) => {
+      //Get reveal position of element
+    let windowHeight = window.innerHeight;
+    let revealtop = element.getBoundingClientRect().top;
+    let revealpoint = 100;
 
-        if(revealPosition(element)) {
-            element.classList.add("fade-up")
-        }
-        else {
-            element.classList.remove("fade-up")
-        }
-    })
+    return revealtop - windowHeight < revealpoint;
+  };
 
-    fadeSide.forEach(element => {
+  fadeUp.forEach((element) => {
+    if (revealPosition(element)) element.classList.add("fade-up");
+  });
 
-        if(revealPosition(element)) {
-            element.classList.add("fade-side")
-        }
-        else {
-            element.classList.remove("fade-side")
-        }
-    })
+  fadeSide.forEach((element) => {
+    if (revealPosition(element)) element.classList.add("fade-side");
+  });
 
-    zoomOut.forEach(element => {
-
-        if(revealPosition(element)) {
-            element.classList.add("zoom-out")
-        }
-        else {
-            element.classList.remove("zoom-out")
-        }
-    })
-    
-
-}
-window.addEventListener("scroll", reveal)
+  zoomOut.forEach((element) => {
+    if (revealPosition(element)) element.classList.add("zoom-out");
+  });
+};
+window.addEventListener("scroll", reveal);
